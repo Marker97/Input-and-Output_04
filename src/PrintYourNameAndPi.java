870295d15a68f345e8f0fdf999ac88218b78b935
Marker97 <chereche10@gmail.com>
2022-11-12 18:03:40 +0100

    Input and Output_04

---
+++ src/PrintYourNameAndPi.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+        String name = "MARCUS";
+        double numberPi =  3.141590;
+        System.out.printf("%s %nThe number Pi is: %f.",name,numberPi);
+    }
+}
