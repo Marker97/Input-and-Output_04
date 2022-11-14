2267639beb82d28aa37bf890eaca7b7ceb4df169
Marker97 <chereche10@gmail.com>
2022-11-14 16:40:13 +0100

    Fix:Input and Output_04

--- src/PrintYourNameAndPi.java
+++ src/PrintYourNameAndPi.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class PrintYourNameAndPi {
     public static void main(String[] args) {
-        String name = "MARCUS";
+        String name = "marcus";
         double numberPi =  3.141590;
-        System.out.printf("%s %nThe number Pi is: %f.",name,numberPi);
+        System.out.printf("%S" + "%nThe number Pi is: %f.",name,numberPi);
     }
 }
